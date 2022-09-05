aa74bdcc5d22987143b2023eddb74e8075605294
mucarb <murilo.rb60@gmail.com>
2022-09-05 20:04:42 -0300

    Project created

---
+++ backend/src/test/java/com/murilo/dsmeta/dsmeta/DsmetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.murilo.dsmeta.dsmeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
