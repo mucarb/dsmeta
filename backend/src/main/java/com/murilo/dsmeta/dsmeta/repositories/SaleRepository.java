ca306a3113b7c2129b5f7aab92d725aeeba9dd59
mucarb <murilo.rb60@gmail.com>
2022-09-07 16:21:18 -0300

    Date select

--- backend/src/main/java/com/murilo/dsmeta/dsmeta/repositories/SaleRepository.java
+++ backend/src/main/java/com/murilo/dsmeta/dsmeta/repositories/SaleRepository.java
@@ -1,9 +1,19 @@
 package com.murilo.dsmeta.dsmeta.repositories;
 
+import java.time.LocalDate;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.murilo.dsmeta.dsmeta.entities.Sale;
 
 public interface SaleRepository extends JpaRepository<Sale, Long> {
+
+	/*
+	 * Exemplo em JPQL: SELECT obj FROM Sale obj WHERE obj.date BETWEEN :min AND
+	 * :max ORDER BY obj.amount DESC
+	 */
+	Page<Sale> findDistinctByDateBetweenOrderByAmountDesc(LocalDate min, LocalDate max, Pageable pageable);
 
 }
