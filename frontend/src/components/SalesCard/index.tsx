f58ee46421b9e244cb085e55ee4986b8ebf0a1d6
mucarb <murilo.rb60@gmail.com>
2022-09-09 16:07:53 -0300

    Sale listing

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -1,11 +1,13 @@
-import NotificationButton from "../NotificationButton";
 import DatePicker from "react-datepicker";
+import NotificationButton from "../NotificationButton";
 
-import "./styles.css";
 import "react-datepicker/dist/react-datepicker.css";
+import "./styles.css";
 
-import { useEffect, useState } from "react";
 import axios from "axios";
+import { useEffect, useState } from "react";
+import { Sale } from "../../models/sale";
+import { BASE_URL } from "../../utils/request";
 
 function SalesCard() {
   const min = new Date(new Date().setDate(new Date().getDate() - 365));
@@ -13,10 +15,11 @@ function SalesCard() {
 
   const [minDate, setMinDate] = useState(min);
   const [maxDate, setMaxDate] = useState(max);
+  const [sales, setSales] = useState<Sale[]>([]);
 
   useEffect(() => {
-    axios.get("http://localhost:8080/sales").then((response) => {
-      console.log(response.data);
+    axios.get(`${BASE_URL}/sales`).then((response) => {
+      setSales(response.data.content);
     });
   }, []);
 
@@ -59,19 +62,25 @@ function SalesCard() {
           </thead>
 
           <tbody>
-            <tr>
-              <td className="show992">#341</td>
-              <td className="show576">28/06/2022</td>
-              <td>Anakin</td>
-              <td className="show992">15</td>
-              <td className="show992">11</td>
-              <td>R$ 55300.00</td>
-              <td>
-                <div className="dsmeta-red-button-container">
-                  <NotificationButton />
-                </div>
-              </td>
-            </tr>
+            {sales.map((sale) => {
+              return (
+                <tr key={sale.id}>
+                  <td className="show992">{sale.id}</td>
+                  <td className="show576">
+                    {new Date(sale.date).toLocaleDateString()}
+                  </td>
+                  <td>{sale.sellerName}</td>
+                  <td className="show992">{sale.visited}</td>
+                  <td className="show992">{sale.deals}</td>
+                  <td>R$ {sale.amount.toFixed(2)}</td>
+                  <td>
+                    <div className="dsmeta-red-button-container">
+                      <NotificationButton />
+                    </div>
+                  </td>
+                </tr>
+              );
+            })}
           </tbody>
         </table>
       </div>
